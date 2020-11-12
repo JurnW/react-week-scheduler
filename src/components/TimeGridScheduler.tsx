8c8ccd01cad15b13cb7339a5e076a1411b7a53cf
Jurn van Wissen <jurnvanwissen@gmail.com>
2020-11-12 19:27:41 +0700

    Reverse locale from props change

--- src/components/TimeGridScheduler.tsx
+++ src/components/TimeGridScheduler.tsx
@@ -10,12 +10,14 @@ import isEqual from 'lodash/isEqual';
 import times from 'lodash/times';
 import React, {
   useCallback,
+  useContext,
   useEffect,
   useMemo,
   useRef,
   useState,
 } from 'react';
 import scrollIntoView from 'scroll-into-view-if-needed';
+import { SchedulerContext } from '../context';
 import { useClickAndDrag } from '../hooks/useClickAndDrag';
 import { useMousetrap } from '../hooks/useMousetrap';
 import {
@@ -59,7 +61,6 @@ export const TimeGridScheduler = React.memo(function TimeGridScheduler({
   eventContentComponent,
   eventRootComponent,
   disabled,
-  locale,
 }: {
   originDate?: Date;
 
@@ -102,17 +103,13 @@ export const TimeGridScheduler = React.memo(function TimeGridScheduler({
    */
   defaultHours?: [number, number];
 
-  /**
-   * The visual grid increments in minutes.
-   * @default 'en''
-   */
-  locale: string;
   onChange(newSchedule: ScheduleType): void;
   onEventClick?: ScheduleProps['onClick'];
   eventContentComponent?: ScheduleProps['eventContentComponent'];
   eventRootComponent?: ScheduleProps['eventRootComponent'];
   disabled?: boolean;
 }) {
+  const { locale } = useContext(SchedulerContext);
   const originDate = useMemo(() => startOfDay(_originDate), [_originDate]);
   const numVerticalCells = MINS_IN_DAY / verticalPrecision;
   const numHorizontalCells = 7 / horizontalPrecision;
